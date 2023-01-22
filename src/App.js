7c97390a1521dee1569cbd4c45136b5d504d8e28
praiz01 <oshilimpraisek@gmail.com>
2023-01-22 18:02:43 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import CartMenu from "./scenes/global/CartMenu";
+import Navbar from "./scenes/global/Navbar";
+import Home from './scenes/home/Home'
+import ItemDetails from "./scenes/itemdetails/ItemDetails";
+
+
+// this function always ensure page scrolls to top
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  },[pathname])
+
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <ScrollToTop/>
+      <Navbar/>
+        <Routes>
+          <Route  path="/" element={<Home/>} />
+          <Route  path="item:itemId" element={<ItemDetails/>} />
+          <Route  path="checkout" element={<Checkout/>} />
+          <Route  path="checkout/success" element={<Confirmation/>} />
+          {/* <Route  path="/" element={<Home/>} /> */}
+        </Routes>
+        <CartMenu />
+      </BrowserRouter>
+      
     </div>
   );
 }
